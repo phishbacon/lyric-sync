1daca9d7f17d7cd445a88195d8a46c5c5185ea35
phishbacon <thechristianreynolds@gmail.com>
2025-12-25 17:16:31 -0500

    chore: make decode/encode functions safe for empty strings

--- src/lib/uuid-encoder.ts
+++ src/lib/uuid-encoder.ts
@@ -1,18 +1,32 @@
 import { logger } from "./logger";
 
 export function encodePlexID(ID: string): string {
+  if (!ID) {
+    return "";
+  }
   logger.info(`ID before encode: ${ID}`);
-  const encoded = btoa(ID).replaceAll("+", "-").replaceAll("/", "_").replaceAll("=", "");
+  const encoded = btoa(ID)
+    .replaceAll("+", "-")
+    .replaceAll("/", "_")
+    .replaceAll("=", "");
   logger.info(`ID after encode: ${encoded}`);
   return encoded;
 }
 
 export function decodePlexID(encoded: string): string {
+  if (!encoded) {
+    return "";
+  }
   // 4 - (encoded.length % 4) how many more chars we need to get to length that
   // is multiple of 4
   // last % 4 is for the case that the length is already a multiple of 4
   logger.info(`ID before decode: ${encoded}`);
-  const decoded = atob(encoded.padEnd(encoded.length + ((4 - (encoded.length % 4)) % 4), "="));
-  logger.info(`ID before decode: ${decoded}`);
+  const decoded = atob(
+    encoded
+      .replaceAll("-", "+")
+      .replaceAll("_", "/")
+      .padEnd(encoded.length + ((4 - (encoded.length % 4)) % 4), "="),
+  );
+  logger.info(`ID after decode: ${decoded}`);
   return decoded;
 }
