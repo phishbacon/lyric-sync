4e09e419093d494788e7a7d6e1fa5b61f0da005e
phishbacon <thechristianreynolds@gmail.com>
2025-12-25 17:18:33 -0500

    chore: use debug for encoder logging

--- src/lib/uuid-encoder.ts
+++ src/lib/uuid-encoder.ts
@@ -4,12 +4,12 @@ export function encodePlexID(ID: string): string {
   if (!ID) {
     return "";
   }
-  logger.info(`ID before encode: ${ID}`);
+  logger.debug(`ID before encode: ${ID}`);
   const encoded = btoa(ID)
     .replaceAll("+", "-")
     .replaceAll("/", "_")
     .replaceAll("=", "");
-  logger.info(`ID after encode: ${encoded}`);
+  logger.debug(`ID after encode: ${encoded}`);
   return encoded;
 }
 
@@ -20,13 +20,13 @@ export function decodePlexID(encoded: string): string {
   // 4 - (encoded.length % 4) how many more chars we need to get to length that
   // is multiple of 4
   // last % 4 is for the case that the length is already a multiple of 4
-  logger.info(`ID before decode: ${encoded}`);
+  logger.debug(`ID before decode: ${encoded}`);
   const decoded = atob(
     encoded
       .replaceAll("-", "+")
       .replaceAll("_", "/")
       .padEnd(encoded.length + ((4 - (encoded.length % 4)) % 4), "="),
   );
-  logger.info(`ID after decode: ${decoded}`);
+  logger.debug(`ID after decode: ${decoded}`);
   return decoded;
 }
